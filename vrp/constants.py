91adab1b859a9d3adf9cd2483c2238ae1eead2ac
sakuraaah <niktro30@gmail.com>
2026-05-09 15:59:31 +0300

    config fixes

--- vrp/constants.py
+++ vrp/constants.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
-
 GRAPH_K = 20
 SHARD_SIZE = 100
 DATASET_NAME = "vrp_training_dataset_v4"
-OUTPUT_NAME = "edge_gnn_v2"
+OUTPUT_NAME = "edge_gnn"
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATASET_DIR = PROJECT_ROOT / "data" / DATASET_NAME
